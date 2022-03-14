90dd1bb873247d19b1c7ba9c760e9fe92cf0d630
luxkun65 <66570610+luxkun65@users.noreply.github.com>
2022-03-14 17:06:58 -0500

    Estructuras de repeticion
    Se agregan ejemplos de estructuras de descicion como lo son do, for y while

---
+++ 6_Estructuras_de_repeticion/While.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace::std;
+
+
+
+int main(){
+
+int x = 1;
+
+while (x){
+
+    if(x%2==0){
+
+        cout<<x<<" es par"<<endl;
+
+    }else{
+
+    cout<<x<<" es impar"<<endl;
+
+    }
+
+    x++;
+}
+return 0;
+}
